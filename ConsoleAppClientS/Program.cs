abdafe561f08380dfb99187e87c760c22fb216ea
Keegan Carlo Falcao <keeganfalcao@outlook.com>
2020-10-10 20:24:46 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleAppClientS/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+/// <summary>
+/// Keegan Carlo Falcao 5F 29/09/2020
+/// </summary>
+
+namespace ConsoleAppClientS
+{
+    class Program
+    {
+        static Socket sender;
+        static private void RicezionePacchettoDaServer(byte[] dataBytes, bool exit)
+        {
+            lock (new object())
+            {
+                string textReceived = "";
+                do
+                {
+                    // Receive the response from the remote device.  
+                    int bytesRec = sender.Receive(dataBytes);
+                    textReceived = Encoding.ASCII.GetString(dataBytes, 0, bytesRec);
+                    Console.WriteLine("<Server>Messaggio Ricevuto: {0}",
+                        textReceived);
+
+                } while (textReceived != "ciao" && sender.Connected);
+
+                if (textReceived == "ciao")
+                    CloseConnection(sender, ref exit);
+                return;
+            }
+        }
+
+        static private void CloseConnection(Socket sender, ref bool exit)
+        {
+            Console.WriteLine($"<Client>Termina la connessione con il Server[IPv6] {sender.RemoteEndPoint}");
+            exit = true;
+            // Release the socket.  
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Keegan Carlo Falcao 5F Autore: Keegan Carlo \n CLIENT***********");
+            // Data buffer for incoming data.  
+            byte[] dataBytes = new byte[1024];
+            string testo = "";
+
+            // Connect to a remote device.  
+            try
+            {
+                // Establish the remote endpoint for the socket.  
+                // This example uses port 11000 on the local computer.  
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[2];
+                IPEndPoint destIP = new IPEndPoint(0, 32000);
+
+                Console.WriteLine($"<Client>Indirizzo host: {ipAddress}");
+
+                // Create a TCP/IP  socket.  
+                sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+                Thread threadClient = null;
+
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                
+                bool exit = false;
+                sender.Connect(destIP);
+
+                do
+                {
+                       
+                    Console.WriteLine("<Client>Inserisci un testo");
+                    testo = Console.ReadLine();
+                    // Encode the data string into a byte array.  
+                    byte[] msg = Encoding.ASCII.GetBytes(testo);
+
+                    // Send the data through the socket.  
+                    int bytesSent = sender.Send(msg);
+                    if (testo != "ciao")
+                    {
+                        threadClient = new Thread(() => RicezionePacchettoDaServer(dataBytes, exit));
+                        threadClient.Start();
+                    }
+
+                    if (testo == "ciao")
+                    {                        
+                        if (threadClient.IsAlive)
+                        {
+                            threadClient.Abort();
+                            threadClient.Join();
+                        }
+                        CloseConnection(sender, ref exit);
+                        break;
+                    }
+
+                }
+                while (!exit);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            //Terminazione 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleAppServerClientSocket/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+/// <summary>
+/// Keegan Carlo Falcao 5F 29/09/2020
+/// </summary>
+
+namespace ConsoleAppServerClientSocket
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //Prefazione
+            Console.WriteLine("Keegan Carlo Falcao 5F Autore: Keegan Carlo" +
+                "\n SERVER***********");
+            //stringa di dati da inviare
+            string data = null;
+            string readMessage = "";
+            byte[] buffer = new byte[1024];
+            //Il dns ottiene il nome dell'host della macchine in locale
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[2];
+            //ottiene l'indirizzo ip della macchina e la sua relativa porta
+            IPEndPoint pEndPoint = new IPEndPoint(0, 32000);
+            //stampa dell'indirizzo locale
+            Console.WriteLine($"Indirizzo host: {ipAddress}");
+
+            //creazione socket TCP
+            Socket ascoltatore = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+         
+            try
+            {
+                //Associazione dell'endpoint al socket
+                ascoltatore.Bind(pEndPoint);
+                //Massimo 10 client in ascolto
+                ascoltatore.Listen(10);
+                //Permette l'Uscita da un ciclo (teoricamente)sempre true
+                bool exit = false;
+                bool superExit = false;
+
+                while (!superExit)
+                {
+                    //Il socket si mette in ascolto
+                    Console.WriteLine("<<Richesta Conessione>>");
+                    Socket handler = ascoltatore.Accept();
+                    Thread threadInvioDaServer = null;
+                    //Sospensione del programma in attesa di richieste
+                    while (!exit)
+                    {
+                        if (threadInvioDaServer != null)
+                        {
+                            threadInvioDaServer.Join();
+                            if (readMessage == "ciao")
+                                break;
+                        }
+                        //Riceve il messaggio con la lunghezza del dato
+                        int nByteRec = handler.Receive(buffer);
+                        data = Encoding.ASCII.GetString(buffer, 0, nByteRec);
+                        //Stampa del messaggio ricevuto dal client
+                        Console.WriteLine($"<Client> Messaggio Ricevuto: {data}");                
+                        //Termina la connessione de riceve la stringa ciao
+                        if (data != "ciao")
+                        {
+                            //Thread riguardo all'invio dei messaggi dal server
+                            threadInvioDaServer = new Thread(() => PacchettoDaServer(out readMessage, handler, exit));
+                            threadInvioDaServer.Start();
+                        }
+                        if (data == "ciao")
+                        {
+                            if (threadInvioDaServer != null)
+                            {
+                                threadInvioDaServer.Abort();
+                                threadInvioDaServer.Join();
+                            }
+                            ServerCloseConnection(handler, ref exit);
+
+                            break;//esce dal ciclo
+                        }
+                        //stringa di conferma messaggio
+                        string ack = "<Server> Conferma ACK";
+                        //Conferma del messaggio Ricevuto
+                        byte[] sendBytes = Encoding.ASCII.GetBytes(ack);
+                        //Invio Messaggio da parte del server
+                        handler.Send(sendBytes);
+                    }
+
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //Terminazione Programma
+            Console.ReadKey();
+
+
+        }
+
+        /// <summary>
+        /// Invio dei messaggi da parte del server
+        /// </summary>
+        /// <param name="msgServ">messaggio da inviare al Client</param>
+        /// <param name="handler">ottiene il socket di cui è stato precedentemente associato</param>
+        /// <param name="exit">permette l'uscita dal ciclo</param>
+        private static void PacchettoDaServer(out string msgServ, Socket handler, bool exit)
+        {
+            //legge una stringa di dati dal Server
+            msgServ = Console.ReadLine();
+            //Invia il messaggio al Client
+            handler.Send(Encoding.ASCII.GetBytes(msgServ));
+            if (msgServ == "ciao")
+                ServerCloseConnection(handler, ref exit);
+        }
+        /// <summary>
+        /// Chiude la connessione con il socket del client
+        /// </summary>
+        /// <param name="handler">ottiene il socket di cui è stato precedentemente associat</param>
+        /// <param name="exit">permette l'uscita dal ciclo</param>
+        static void ServerCloseConnection(Socket handler, ref bool exit)
+        {           
+            Console.WriteLine($"<Server>Termina la conessione con il Client[IPv6] {handler.RemoteEndPoint}");
+            //Non permette più l'invio e la ricezione di messaggi
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+            //permette l'uscita del ciclo degli invii e delle ricezioni
+            exit = true;
+        }
+    }
+}
